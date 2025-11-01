77e1c0fe85c107f794ec22345387b34defd9100d
thevibecoder13 <akshitamahapatro13@gmail.com>
2025-11-01 11:46:10 +0530

    js basics

---
+++ 01_basics/jsbasics.js
@@ -0,0 +1,10 @@
+//console.log("Hello World");
+// let namee = "Akshita";
+// let age = 18;
+// console.log(namee, age);
+// console.table([namee, age]);
+// console.log(typeof age);
+let  a = 2;
+let b="2";
+console.log(a==b);// here it compares both a and b w/o type
+console.log(a===b); //here it compares both a and b with type
